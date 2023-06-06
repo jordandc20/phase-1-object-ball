6877b03fd39a975602d0ecc7ef5361d16b460f43
Diana Jordan <dianacjordan@gmail.com>
2023-06-06 19:55:28 -0400

    finish

---
+++ src/00-objectball.js
@@ -0,0 +1,71 @@
+function gameObject () {
+    return {
+
+
+    home: {
+        teamName: 'Brooklyn Nets',
+        colors: ['Black', 'White'],
+        players: {
+            'Alan Anderson': {
+                number: 0,
+                shoe: 16,
+                points: 22,
+                rebounds: 12,
+                assists: 12,
+                steals: 3,
+                blocks: 1,
+                slamDunks: 1
+            },
+            'Reggie Evans': {
+                number: 30,
+                shoe: 14,
+                points: 12,
+                rebounds: 12,
+                assists: 12,
+                steals: 12,
+                blocks: 12,
+                slamDunks: 7
+            },
+            // 'Brook Lopez':,
+            // 'Mason Plumlee':,
+            // 'Jason Terry':,
+        }
+    },
+
+
+    away: {
+        teamName: 'Charlotte Hornets',
+        colors: ['Turquoise', 'Purple'],
+        players: {
+            'Jeff Adrien': {
+                number: 4,
+                shoe: 18,
+                points: 10,
+                rebounds: 1,
+                assists: 1,
+                steals: 2,
+                blocks: 7,
+                slamDunks: 2,
+            },
+            'Bismak Biyombo': {
+                number: 0,
+                shoe: 16,
+                points: 12,
+                rebounds: 4,
+                assists: 7,
+                steals: 7,
+                blocks: 15,
+                slamDunks: 10,
+            },
+            // 'DeSagna Diop':,
+            //     'Ben Gordon'	:,
+            // 'Brendan Haywood':
+
+
+        }
+
+
+
+    }
+}}
+
